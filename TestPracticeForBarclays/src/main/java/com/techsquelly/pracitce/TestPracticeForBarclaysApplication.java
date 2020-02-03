7a1469b076bac242f49a40e31d3775d1ee407f12
sanjaysaini <sanjaysaini@172.20.10.3>
2020-02-03 14:01:02 +0530

    first files

---
+++ TestPracticeForBarclays/src/main/java/com/techsquelly/pracitce/TestPracticeForBarclaysApplication.java
@@ -0,0 +1,13 @@
+package com.techsquelly.pracitce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPracticeForBarclaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPracticeForBarclaysApplication.class, args);
+	}
+
+}
