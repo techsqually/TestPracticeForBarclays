a7353a2096183748b7f7ac8554ffaac0bc86ebbb
sanjaysaini <sanjaysaini@172.20.10.3>
2020-02-03 15:25:31 +0530

    commiting first two test cases

--- TestPracticeForBarclays/src/test/java/com/techsquelly/pracitce/TestPracticeForBarclaysApplicationTests.java
+++ TestPracticeForBarclays/src/test/java/com/techsquelly/pracitce/TestPracticeForBarclaysApplicationTests.java
@@ -1,5 +1,6 @@
 package com.techsquelly.pracitce;
 
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,11 @@ class TestPracticeForBarclaysApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testCase1(){
+		org.springframework.util.Assert.isTrue(true);
 	}
 
 }
